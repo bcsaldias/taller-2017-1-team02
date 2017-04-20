e60df9d82261e6dc0f1694da90bc59281ac96d86
bcsaldias <bcsaldias@uc.cl>
2017-04-20 00:05:53 -0300

    Added first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   get 'api/prices'
+  get 'api/oc/recibir/:id' => 'api#recibir_oc'
+  get 'api/factura/recibir/:id' => 'api#recibir_factura'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
