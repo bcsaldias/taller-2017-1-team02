ae6ae31ebf9e869925169772bad6d135b70e5a13
bcsaldias <bcsaldias@uc.cl>
2017-04-20 00:05:53 -0300

    Added first routes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,32 @@
+require 'json'
+require 'net/http'
+
 class ApiController < ApplicationController
   def prices
-    json_response("SHAKIRA")
+    json_response("lista de precios")
   end
+
+  # POST /api/oc/recibir
+  def recibir_oc
+    json_response("se ha recibido la OC: "+params[:id])
+  end
+
+  # GET /sistema_central
+  def get_oc
+  	# ir al servidor
+    json_response("se ha ido a buscar la OC: "+params[:id])
+  end
+
+  # POST /api/factura/recibir
+  def recibir_factura
+    json_response("se ha recibido la factura: "+params[:id])
+  end
+
+  # GET /sistema_central
+  def get_factura
+    json_response("se ha ido a buscar la factura: "+params[:id])
+  end
+
+
+
 end
