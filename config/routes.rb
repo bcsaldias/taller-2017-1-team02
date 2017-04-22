6cc4d045c0588b80bd48b386664b3821c1b882e8
bcsaldias <bcsaldias@uc.cl>
2017-04-22 12:40:04 -0300

    Fixed invoice routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'invoice/enviar_confirmacion_factura'
-  get 'invoice/enviar_rechazo_factura'
-  get 'invoice/enviar_confirmacion_pago'
-  get 'invoice/enviar_factura'
-  get 'invoice/notificar_orden_despachada'
-
   get 'products' => 'api#products'
-  
+
+  put 'invoices/:id' => 'invoice#enviar_confirmacion_factura'
+  delete 'invoices/:id' => 'invoice#enviar_rechazo_factura'
+  patch 'invoices/:id' => 'invoice#enviar_confirmacion_pago'
+  put 'invoices' => 'invoice#enviar_factura'
+  patch 'invoices/:id' => 'invoice#notificar_orden_despachada'
+
   get 'api/oc/recibir/:id' => 'api#recibir_oc'
   get 'api/factura/recibir/:id' => 'api#recibir_factura'
 
