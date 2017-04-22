705c38442b0957359bd324ca341796c5af24a9dc
bcsaldias <bcsaldias@uc.cl>
2017-04-22 12:51:43 -0300

    Added purchase orders controller

---
+++ app/controllers/purchase_order_controller.rb
@@ -0,0 +1,11 @@
+class PurchaseOrderController < ApplicationController
+
+  # PUT purchase_orders/:id
+  def realizar_pedido
+  end
+
+  # PATCH purchase_orders/:id
+  def responder_orden_compra
+  end
+  
+end
