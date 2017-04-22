5a1ec6d0676b1458152989d3ea0435dc5f2d6d5d
bcsaldias <bcsaldias@uc.cl>
2017-04-22 12:40:04 -0300

    Fixed invoice routes

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -1,16 +1,23 @@
 class InvoiceController < ApplicationController
+
+  # PUT /invoices/:id
   def enviar_confirmacion_factura
   end
 
+  # DELETE /invoices/:id
   def enviar_rechazo_factura
   end
 
+  # PATCH /invoices/:id
   def enviar_confirmacion_pago
   end
 
+  # PUT /invoices
   def enviar_factura
   end
 
+  # PATCH /invoices/:id
   def notificar_orden_despachada
   end
 end
+
