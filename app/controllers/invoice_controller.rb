6e9a7c6452c7814e18ce978b0d8d9772dc412be7
bcsaldias <bcsaldias@uc.cl>
2017-04-22 12:29:32 -0300

    Added invoice controller

---
+++ app/controllers/invoice_controller.rb
@@ -0,0 +1,16 @@
+class InvoiceController < ApplicationController
+  def enviar_confirmacion_factura
+  end
+
+  def enviar_rechazo_factura
+  end
+
+  def enviar_confirmacion_pago
+  end
+
+  def enviar_factura
+  end
+
+  def notificar_orden_despachada
+  end
+end
