4e17fb39121c463714e1919313dfd7bc51e80da4
bcsaldias <bcsaldias@uc.cl>
2017-04-22 12:55:20 -0300

    Fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'products' => 'api#products'
 
   put 'purchase_orders/:id' => 'purchase_order#realizar_pedido'
-  patch 'purchase_orders/:id' => 'purchase_order/responder_orden_compra'
+  patch 'purchase_orders/:id' => 'purchase_order#responder_orden_compra'
 
   put 'invoices/:id' => 'invoice#enviar_confirmacion_factura'
   delete 'invoices/:id' => 'invoice#enviar_rechazo_factura'
