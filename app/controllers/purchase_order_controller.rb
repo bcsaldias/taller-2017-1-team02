724108b09181677a41dc0797592f3cc860036713
Joaquin Sotomayor <joaquinsotomayorc@gmail.com>
2017-04-22 22:36:45 -0300

    Added mock responses for Proceso de Ventas

--- app/controllers/purchase_order_controller.rb
+++ app/controllers/purchase_order_controller.rb
@@ -1,11 +1,23 @@
 class PurchaseOrderController < ApplicationController
 
+  require 'json'
+
   # PUT purchase_orders/:id
   def realizar_pedido
-  end
+    @body = JSON.parse request.body.read
+    @keys = @body.keys
 
+    if not @keys.include?("payment_method")
+      json_response ({ error: "Falta método de pago"}), 400
+    elsif not @keys.include?("id_store_reception")
+      json_response ({error: "Falta bodega de recepción"}), 400
+    else
+      render json: {orden: 15}, status: 201
+    end
+
+  end
   # PATCH purchase_orders/:id
   def responder_orden_compra
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
   put 'purchase_orders/:id' => 'purchase_order#realizar_pedido'
   patch 'purchase_orders/:id' => 'purchase_order#responder_orden_compra'
 
-  put 'invoices/:id' => 'invoice#enviar_confirmacion_factura'
-  delete 'invoices/:id' => 'invoice#enviar_rechazo_factura'
-  patch 'invoices/:id' => 'invoice#enviar_confirmacion_pago'
+  put 'invoices/:id/accepted' => 'invoice#enviar_confirmacion_factura'
+  delete 'invoices/:id/rejected' => 'invoice#enviar_rechazo_factura'
+  patch 'invoices/:id/paid' => 'invoice#enviar_confirmacion_pago'
   put 'invoices' => 'invoice#enviar_factura'
   patch 'invoices/:id' => 'invoice#notificar_orden_despachada'
 
-  patch 'suppliers/:id' => 'suppliers/informar_cuenta_banco'
+  patch 'suppliers/:id' => 'suppliers#informar_cuenta_banco'
 
 
 end
