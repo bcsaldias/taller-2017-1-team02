176a24a5f7b89779baa70c6ea91167839bc91a59
Rosario Calderon <rvcalderon@uc.cl>
2017-05-13 19:12:52 -0300

    Addapted reject and accept OC

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,5 +1,5 @@
 class PurchaseOrder < ApplicationRecord
-  enum state: [:creada, :aceptada, :rechazada, :finalizada, :anulada]
+  enum state: [:creada, :aceptada, :rechazada, :finalizada]
 
   belongs_to :product, foreign_key: :product_sku
   has_one :invoice
