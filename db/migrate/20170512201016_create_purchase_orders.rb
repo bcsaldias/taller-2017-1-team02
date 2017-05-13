12f067439f01da348d2e081092587c8e5882852f
bcsaldias <bcsaldias@uc.cl>
2017-05-13 15:01:54 -0300

    Added realizar_pedido method

--- db/migrate/20170512201016_create_purchase_orders.rb
+++ db/migrate/20170512201016_create_purchase_orders.rb
@@ -2,7 +2,7 @@ class CreatePurchaseOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :purchase_orders do |t|
     	
-      t.integer :id_cloud, null: false
+      t.string :id_cloud, null: false
       t.integer :state
       t.string :product_sku, foreign_key: true, null: false
       t.string :id_store_reception
