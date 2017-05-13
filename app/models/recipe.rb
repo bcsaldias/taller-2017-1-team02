719a33824f8877e297efa80bbf0fffb72149e5e0
Joaquin Sotomayor <joaquinsotomayorc@gmail.com>
2017-05-13 13:01:08 -0300

    Merged

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
-  belongs_to :final_product, :class_name => 'Product', foreign_key: :final_product_id
-  belongs_to :needed_product, :class_name => 'Product', foreign_key: :needed_product_id
+  belongs_to :final_product, :class_name => 'Product', foreign_key: :final_product_sku
+  belongs_to :needed_product, :class_name => 'Product', foreign_key: :needed_product_sku
 end
