ee9885d3be62f2232e0946e7e2d207af28b42a6e
bcsaldias <bcsaldias@uc.cl>
2017-05-13 11:36:18 -0300

    Added move to external

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,9 +8,15 @@ class ApiController < ApplicationController
 
 	def test
 		#ret = Factory.fabricate_without_paying("2dwwd", 150)
-		ret = Factory.get_account
-		#ret = Production.move_stock("590baa76d6b4ec000490255e","590baa76d6b4ec0004902625")
+		#ret = Factory.get_account
+		#ret = Production.move_stock_external("590baa76d6b4ec000490255d",
+		#									"590baa76d6b4ec000490262a",
+		#									 "32", 1)
 		#ret = Production.get_stock("590baa76d6b4ec000490255f", 41)
+		#ret = Production.get_warehouses
+		#ret = Production.get_stock("590baa76d6b4ec000490255f","41")
+		#ret = Production.move_stock("590baa76d6b4ec000490255e", "590baa76d6b4ec000490262a")
+		#ret = Production.get_stock("590baa76d6b4ec000490255e", "41")
 		json_response({response: ret})
 	end
 
