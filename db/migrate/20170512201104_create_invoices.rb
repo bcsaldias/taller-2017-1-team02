01782235751c9406ecdecaf2ac802575fc1d1f4b
bcsaldias <bcsaldias@uc.cl>
2017-05-14 17:39:51 -0400

    Added contrain unique id cloud

--- db/migrate/20170512201104_create_invoices.rb
+++ db/migrate/20170512201104_create_invoices.rb
@@ -1,7 +1,7 @@
 class CreateInvoices < ActiveRecord::Migration[5.0]
   def change
     create_table :invoices do |t|
-      t.integer :id_cloud, null: false
+      t.integer :id_cloud, null: false, unique: true
       t.integer :state
       t.references :purchase_order, foreign_key: true, null: false
 
