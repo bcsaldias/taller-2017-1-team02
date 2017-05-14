974f9bcfff6eee6e0b29614fd67ea0620e9a1958
bcsaldias <bcsaldias@uc.cl>
2017-05-14 17:39:51 -0400

    Added contrain unique id cloud

--- db/migrate/20170512201152_create_warehouses.rb
+++ db/migrate/20170512201152_create_warehouses.rb
@@ -1,7 +1,7 @@
 class CreateWarehouses < ActiveRecord::Migration[5.0]
   def change
     create_table :warehouses do |t|
-      t.string :id_cloud, null: false
+      t.string :id_cloud, null: false, unique: true
 
       t.timestamps
     end
