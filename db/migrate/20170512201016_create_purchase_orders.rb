5fa2f96d48d13ca42e039a1b890341c9d5f50afa
bcsaldias <bcsaldias@uc.cl>
2017-05-14 17:39:04 -0400

    Added contrain unique id cloud

--- db/migrate/20170512201016_create_purchase_orders.rb
+++ db/migrate/20170512201016_create_purchase_orders.rb
@@ -2,7 +2,7 @@ class CreatePurchaseOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :purchase_orders do |t|
 
-      t.string :id_cloud, null: false
+      t.string :id_cloud, null: false, unique: true
       t.integer :state
       t.string :product_sku, foreign_key: true, null: false
       t.string :id_store_reception
