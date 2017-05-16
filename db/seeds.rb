9954fd3ab80587d51407dfe8bf9b7a35a4aceb8b
bcsaldias <bcsaldias@uc.cl>
2017-05-16 17:50:49 -0400

    Fixed supp

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Warehouse.all.delete_all
-warehouses_prod = Warehouse.create([
-		{id_cloud: "5910c0b90e42840004f6e74a"} , #recepcion
-		{id_cloud: "5910c0b90e42840004f6e74b"} , #despacho
-		{id_cloud: "5910c0b90e42840004f6e770"} , #general
-		{id_cloud: "5910c0b90e42840004f6e74c"} , #general
-		{id_cloud: "5910c0b90e42840004f6e771"} 	 #pulmon
-	])
+PurchaseOrder.all.delete_all
