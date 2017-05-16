aa8b3ff0b822727ceb9522b869c97bc4c5ee7aa4
bcsaldias <bcsaldias@uc.cl>
2017-05-16 16:17:10 -0400

    Updated view manager

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -5,4 +5,22 @@ class PurchaseOrder < ApplicationRecord
   has_one :invoice
   belongs_to :supplier, foreign_key: :supplier_id
 
+
+  def self.our_oc
+    PurchaseOrder.all.where(owner: true).each do |item|
+        item.cantidad
+    end
+  end
+
+
+  def cantidad
+  	order = Sales.get_purchase_order(self.id_cloud)
+  	order['cantidad']
+  end
+
+  def deadline
+  	order = Sales.get_purchase_order(self.id_cloud)
+  	order['fechaEntrega']
+  end
+
 end
