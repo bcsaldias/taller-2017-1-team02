6fe11c7bf29229494c7a87e3655d842aa2eea0a4
bcsaldias <bcsaldias@uc.cl>
2017-05-15 22:25:01 -0400

    Added login/logout

--- Gemfile
+++ Gemfile
@@ -29,3 +29,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'apipie-rails'
+
+# Gemfile
+gem 'sorcery'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
     include ExceptionHandler
     include Production
     include Queries
-		include RawMaterial
-		include Tiempo
+	include RawMaterial
+	include Tiempo
+
+	private
+	def not_authenticated
+	  redirect_to login_path, alert: "Please login first"
+	end
+	
 end
