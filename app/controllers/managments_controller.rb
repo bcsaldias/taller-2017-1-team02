c08efc07f1e005166fb0a92cdc7bb56c1e0b337f
bcsaldias <bcsaldias@uc.cl>
2017-05-16 16:24:15 -0400

    Updated time

--- app/controllers/managments_controller.rb
+++ app/controllers/managments_controller.rb
@@ -27,7 +27,7 @@ class ManagmentsController < ApplicationController
     hora = params[:fecha_hora]
     minutos = params[:fecha_minutos]
     comprar = RawMaterial.buy_product_from_supplier(params[:oc_sku], params[:cantidad].to_i, params[:proveedor].to_i, 
-                          needed_date = Tiempo.tiempo_a_milisegundos(5, 16, 20, 00)) #mes, dia, hora, minuto
+                          needed_date = Tiempo.tiempo_a_milisegundos(5, 16, 21, 00)) #mes, dia, hora, minuto
 
   	redirect_to :managment
   end
