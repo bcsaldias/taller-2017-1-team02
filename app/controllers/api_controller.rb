766f93b3e17a8bf27c483212bf2f6ed211e19339
bcsaldias <bcsaldias@uc.cl>
2017-05-16 20:11:15 -0400

    Fixed lote minimo

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,13 +5,13 @@ class ApiController < ApplicationController
 
 	include Queries
 	def test
-		ret =     comprar = RawMaterial.buy_product_from_supplier("7", 50, "7", 
+		ret =     comprar = RawMaterial.buy_product_from_supplier("7", 50, 7, 
                           needed_date = Tiempo.tiempo_a_milisegundos(5, 16, 22, 00)) #mes, dia, hora, minuto
 		json_response({response: ret})
 	end
 
 	def test2
-		ret =     comprar = RawMaterial.buy_product_from_supplier("7", 50, "2", 
+		ret =     comprar = RawMaterial.buy_product_from_supplier("7", 50, 2, 
                           needed_date = Tiempo.tiempo_a_milisegundos(5, 16, 22, 00)) #mes, dia, hora, minuto
 		json_response({response: ret})
 	end
