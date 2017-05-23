0b6e3fe3abd314e9785cd11da39bee209237a212
bcsaldias <bcsaldias@uc.cl>
2017-05-22 20:13:11 -0400

    Added stock to products view

---
+++ app/overrides/add_sku_to_product_view.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path: 'spree/products/show',
+  name: 'add_sku_to_product_view',
+  insert_after: "[data-hook='cart_form']",
+  text: "<h4><%= 'Stock disponible: ' + @product.total_on_hand.to_s %></h4>")
