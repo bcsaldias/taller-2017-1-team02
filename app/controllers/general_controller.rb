6a5d625ad4398879aaea2b091161b8879ae4e759
Rosario Calderon <rvcalderon@uc.cl>
2017-06-01 22:09:47 -0300

    Added column sort invoices, production, transaction, vouchers

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -31,31 +31,31 @@ class GeneralController < ApplicationController
   end
 
   def vouchers
-    @vouchers = Voucher.all
+    @vouchers = Voucher.all.order(sort_column(Voucher, "id_cloud") + " " + sort_direction)
 
   end
 
   def oc
     # J: Busca localmente las POrders
-    @our_purchase_orders = PurchaseOrder.where(owner: true).order(sort_column + " " + sort_direction)
-    @purchase_orders = PurchaseOrder.where(owner: nil).order(sort_column + " " + sort_direction)
+    @our_purchase_orders = PurchaseOrder.where(owner: true).order(sort_column(PurchaseOrder, "product_sku") + " " + sort_direction)
+    @purchase_orders = PurchaseOrder.where(owner: nil).order(sort_column(PurchaseOrder, "product_sku") + " " + sort_direction)
 
   end
 
   def invoices
-    @our_invoices = Invoice.where(owner: true)
-    @invoices = Invoice.where(owner: nil)
+    @our_invoices = Invoice.where(owner: true).order(sort_column(Invoice, "id_cloud") + " " + sort_direction)
+    @invoices = Invoice.where(owner: nil).order(sort_column(Invoice, "id_cloud") + " " + sort_direction)
   end
 
   def transaction
-    @transactions_ok = Transaction.where(state: true)
-    @transactions_fail = Transaction.where(state: false)
+    @transactions_ok = Transaction.where(state: true).order(sort_column(Transaction, "id_cloud") + " " + sort_direction)
+    @transactions_fail = Transaction.where(state: false).order(sort_column(Transaction, "id_cloud") + " " + sort_direction)
   end
 
   def production
-    @personal_account = ProductionOrder.all
-    @production_orders = ProductionOrder.all
-    @factory = ProductionOrder.all
+    @personal_account = ProductionOrder.all.order(sort_column(ProductionOrder, "product_sku") + " " + sort_direction)
+    @production_orders = ProductionOrder.all.order(sort_column(ProductionOrder, "product_sku") + " " + sort_direction)
+    @factory = ProductionOrder.all.order(sort_column(ProductionOrder, "product_sku") + " " + sort_direction)
   end
 
   def authorize
@@ -63,10 +63,13 @@ class GeneralController < ApplicationController
   end
 
   private
+
+  # CAMBIAR A INDICE !!
   
-  def sort_column
-    PurchaseOrder.column_names.include?(params[:sort]) ? params[:sort] : "product_sku"
+  def sort_column(nombre, defecto)
+    nombre.column_names.include?(params[:sort]) ? params[:sort] : defecto
   end
+
   
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
