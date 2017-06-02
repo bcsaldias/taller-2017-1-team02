bd3973a3604a739275b98e5e6db90c0fdaa19945
Rosario Calderon <rvcalderon@uc.cl>
2017-06-01 21:07:01 -0300

    Added comm cee797ab6cf417cf0bf25d6a21929a6189733739

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,7 +1,7 @@
 #include HTTParty
 class GeneralController < ApplicationController
   before_action :authorize
-  helper_method :sort_wh, :notify_deliver, :deliver, :check_for_availablility, :create_oc, :accept_oc
+  helper_method :sort_wh, :notify_deliver, :deliver, :check_for_availablility, :create_oc, :accept_oc, :sort_column, :sort_direction
   include Warehouses
   include RawMaterial
   require 'json'
@@ -29,8 +29,44 @@ class GeneralController < ApplicationController
     
   end
 
+<<<<<<< HEAD
+=======
+  def oc
+    # J: Busca localmente las POrders
+    @our_purchase_orders = PurchaseOrder.where(owner: true).order(sort_column + " " + sort_direction)
+    @purchase_orders = PurchaseOrder.where(owner: nil).order(sort_column + " " + sort_direction)
+
+  end
+
+  def invoices
+    @our_invoices = Invoice.where(owner: true)
+    @invoices = Invoice.where(owner: nil)
+  end
+
+  def transaction
+    @transactions_ok = Transaction.where(state: true)
+    @transactions_fail = Transaction.where(state: false)
+  end
+
+  def production
+    @personal_account = ProductionOrder.all
+    @production_orders = ProductionOrder.all
+    @factory = ProductionOrder.all
+  end
+
+>>>>>>> cee797a... Added column sort PurchaseOrder
   def authorize
     redirect_to '/login' unless current_user
+  end
+
+  private
+  
+  def sort_column
+    PurchaseOrder.column_names.include?(params[:sort]) ? params[:sort] : "product_sku"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
