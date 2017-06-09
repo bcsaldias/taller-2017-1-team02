f59c40cbbcf526886cfc966351fe1848c2ed80db
Joaquin Sotomayor <joaquinsotomayorc@gmail.com>
2017-06-09 15:15:07 -0400

    Changed transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530144970) do
+ActiveRecord::Schema.define(version: 20170609191139) do
 
   create_table "contacts", force: :cascade do |t|
     t.integer  "product_id"
@@ -1134,11 +1134,11 @@ ActiveRecord::Schema.define(version: 20170530144970) do
     t.string   "id_cloud"
     t.string   "origen"
     t.string   "destino"
-    t.string   "monto"
     t.boolean  "owner"
     t.boolean  "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal  "monto"
   end
 
   create_table "users", force: :cascade do |t|
