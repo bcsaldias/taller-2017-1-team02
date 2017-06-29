0a2cebdcbdb9086325865b95809a693f31557d9e
bcsaldias <bcsaldias@uc.cl>
2017-06-28 23:44:25 -0400

    Fixed mig

--- db/migrate/20180718081616_change_prices_spree.rb
+++ db/migrate/20180718081616_change_prices_spree.rb
@@ -3,14 +3,14 @@ class ChangePricesSpree < ActiveRecord::Migration[5.0]
     Spree::Product.all.each do |product|
       product.price = 184 if product.name == "Huevo"
       product.price = 16546 if product.name == "Crema"
-      product.price = 454 if product.sku == "Trigo"
-      product.price = 533 if product.sku == "Cebada"
-      product.price = 310 if product.sku == "Cacao"
-      product.price = 178 if product.sku == "Sal"
-      product.price = 419 if product.sku == "Uva"
-      product.price = 11470 if product.sku == "Queso"
-      product.price = 9768 if product.sku == "Suero de Leche"
-      product.price = 1803 if product.sku == "Leche Descremada"
+      product.price = 454 if product.name == "Trigo"
+      product.price = 533 if product.name == "Cebada"
+      product.price = 310 if product.name == "Cacao"
+      product.price = 178 if product.name == "Sal"
+      product.price = 419 if product.name == "Uva"
+      product.price = 11470 if product.name == "Queso"
+      product.price = 9768 if product.name == "Suero de Leche"
+      product.price = 1803 if product.name == "Leche Descremada"
       product.save!
     end
   end
