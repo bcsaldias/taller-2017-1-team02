791d1ae2dbf8dd3917c7585565354a14d3a0f9d8
bcsaldias <bcsaldias@uc.cl>
2017-06-29 00:24:21 -0400

    Added two prices

--- app/controllers/concerns/warehouses.rb
+++ app/controllers/concerns/warehouses.rb
@@ -623,10 +623,10 @@ module Warehouses
 
     #ordenes de produccion
     production_orders = ProductionOrder.where("disponible > ?",Time.now)
-    cantidad_deseada= {7 => 4500, 2 => 2000,
-                       6=> 500, 8 => 2000,
-                       14=> 1700, 20=> 2000,
-                       26=> 2000, 39=> 2000,
+    cantidad_deseada= {7 => 4500, 2 => 600,
+                       6=> 500, 8 => 1600,
+                       14=> 1700, 20=> 1600,
+                       26=> 600, 39=> 600,
                        40=> 500, 41=> 800,
                        49=> 800}
     #cant_minima = 25% cant_deseada
---
+++ db/migrate/20180718082616_change_prices_twise.rb
@@ -0,0 +1,33 @@
+class ChangePricesSpree < ActiveRecord::Migration[5.0]
+  def change
+
+    Product.all.each do |product|
+      product.price = 184*2 if product.sku == "2"
+      product.price = 16546*2 if product.sku == "6"
+      product.price = 454*2 if product.sku == "8"
+      product.price = 533*2 if product.sku == "14"
+      product.price = 310*2 if product.sku == "20"
+      product.price = 178*2 if product.sku == "26"
+      product.price = 419*2 if product.sku == "39"
+      product.price = 11470*2 if product.sku == "40"
+      product.price = 9768*2 if product.sku == "41"
+      product.price = 1803*2 if product.sku == "49"
+      product.save!
+    end
+
+    Spree::Product.all.each do |product|
+      product.price = 184*2 if product.name == "Huevo"
+      product.price = 16546*2 if product.name == "Crema"
+      product.price = 454*2 if product.name == "Trigo"
+      product.price = 533*2 if product.name == "Cebada"
+      product.price = 310*2 if product.name == "Cacao"
+      product.price = 178*2 if product.name == "Sal"
+      product.price = 419*2 if product.name == "Uva"
+      product.price = 11470*2 if product.name == "Queso"
+      product.price = 9768*2 if product.name == "Suero de Leche"
+      product.price = 1803*2 if product.name == "Leche Descremada"
+      product.save!
+    end
+  end
+end
+
