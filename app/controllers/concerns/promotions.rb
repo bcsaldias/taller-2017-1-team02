e83f1c9e81dc787e68c1c072ceda761da31ec064
bcsaldias <bcsaldias@uc.cl>
2017-06-28 21:50:24 -0400

    Added true uries

--- app/controllers/concerns/promotions.rb
+++ app/controllers/concerns/promotions.rb
@@ -13,9 +13,9 @@ module Promotions
     product = Product.find(promotion.sku)
     inicio = promotion.inicio.strftime("%d/%b a las %I:%M%p")
     fin = promotion.fin.strftime("%d/%b a las %I:%M%p")
-    env_path = Rails.configuration.environment_ids['our_env_path']
+    ecommerce_uri = Rails.configuration.environment_ids['ecommerce_uri']
     return  ("#{product.description} a $#{promotion.precio}. Usa '#{promotion.codigo}'. 
-            Desde #{inicio} a #{fin}."+"Ven https://goo.gl/EyzEYq !")[0,140]
+            Desde #{inicio} a #{fin}."+"Ven #{ecommerce_uri} !")[0,140]
   end
 
   def self.get_promo_picture(promotion)
